b6a28abb751b1f4d36d71dc069728b77e7b5a96a
Théo Le Calvar <goldensuneur@gmail.com>
2015-03-03 09:09:06 +0100

    Correction in VA_ARG

--- src/log.h
+++ src/log.h
@@ -1,28 +1,39 @@
 #ifndef _log_h_
 #define _log_h_
 
+#define INFO_LEVEL 0x8
+#define WARN_LEVEL 0x4
+#define ERR_LEVEL  0x2
+
+#ifndef LOG_LEVEL
+#define LOG_LEVEL  0x2
+#endif
+
 #ifdef DEBUG
 #define log(_type, _stream, _format, ...) \
         fprintf(_stream, \
                 _type": %s:%d in %s: "_format"\n", \
-                __FILE__, __LINE__, __FUNCTION__, __VA_ARGS__); \
+                __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__); \
                 fflush(_stream);
 #else
 #define log(_type, _stream, _format, ...) \
         fprintf(_stream, \
                 _format"\n", \
-                __VA_ARGS__); \
+                ##__VA_ARGS__); \
                 fflush(_stream);
 #endif
 
 #define log_err(_format, ...) \
-        log("error", stderr, _format, __VA_ARGS__)
+        if (LOG_LEVEL >= ERR_LEVEL) \
+        log("error", stderr, "\033[31m" _format "\033[0m", ##__VA_ARGS__)
 
 #define log_info(_format, ...) \
-        log("info", stderr, _format, __VA_ARGS__)
+        if (LOG_LEVEL >= WARN_LEVEL) \
+        log("info", stderr, "\033[32m" _format "\033[0m", ##__VA_ARGS__)
 
 #define log_warn(_format, ...) \
-        log("warning", stdout, _format, __VA_ARGS__)
+        if (LOG_LEVEL >= INFO_LEVEL) \
+        log("warning", stdout, "\033[33m" _format "\033[0m", ##__VA_ARGS__)
 
 #endif
 
