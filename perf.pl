55e040bafc1b3b0cdd20a628ad1776ecbbdbfb95
Théo Le Calvar <goldensuneur@gmail.com>
2015-03-05 12:10:57 +0100

    New tool for benchmark

--- perf.pl
+++ perf.pl
@@ -2,17 +2,26 @@
 
 use strict;
 
+my $function = 'bt';
+
+if ($#ARGV+1 > 0)  {
+        $function = $ARGV[0];
+}
+
 my $outFile;
 
-open $outFile, '>>', 'results.txt' or die "Can't open resultats.txt\n";
+open $outFile, '>', 'results.txt' or die "Can't open resultats.txt\n";
+
+print{$outFile}("$function\nsize;realtime;res\n");
+print("$function\nsize;realtime;res\n");
 
 for (my $size = 4; $size < 50; $size++) {
-        my $output = `gtime -p ./n-dames -a bt $size 2>&1`;
+        my $output = `gtime -p ./n-dames -a $function $size 2>&1`;
 
         my ($res, $realTime, $userTime) = ($output =~ /(.*?)\nreal (.*?)\nuser (.*?)\nsys \d/);
 
         $res =~ tr/ //d;
 
-        print{$outFile}("$size;$realTime;$userTime;$res\n");
-        print("$size;$realTime;$userTime;$res\n");
+        print{$outFile}("$size;$realTime;$res\n");
+        print("$size;$realTime;$res\n");
 }
