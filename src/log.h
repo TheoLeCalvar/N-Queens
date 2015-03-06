7783777af33e844d29ef94e03fa53fede01e48c9
Théo Le Calvar <theo.lecalvar@gmail.com>
2015-03-06 18:17:37 +0100

    We have to change the log macro

--- src/log.h
+++ src/log.h
@@ -10,13 +10,13 @@
 #endif
 
 #ifdef DEBUG
-#define log(_type, _stream, _format, ...) \
+#define __log(_type, _stream, _format, ...) \
         fprintf(_stream, \
                 _type": %s:%d in %s: "_format"\n", \
                 __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__); \
                 fflush(_stream);
 #else
-#define log(_type, _stream, _format, ...) \
+#define __log(_type, _stream, _format, ...) \
         fprintf(_stream, \
                 _format"\n", \
                 ##__VA_ARGS__); \
@@ -25,15 +25,15 @@
 
 #define log_err(_format, ...) \
         if (LOG_LEVEL >= ERR_LEVEL) \
-        log("error", stderr, "\033[31m" _format "\033[0m", ##__VA_ARGS__)
+        __log("error", stderr, "\033[31m" _format "\033[0m", ##__VA_ARGS__)
 
 #define log_info(_format, ...) \
         if (LOG_LEVEL >= WARN_LEVEL) \
-        log("info", stderr, "\033[32m" _format "\033[0m", ##__VA_ARGS__)
+        __log("info", stderr, "\033[32m" _format "\033[0m", ##__VA_ARGS__)
 
 #define log_warn(_format, ...) \
         if (LOG_LEVEL >= INFO_LEVEL) \
-        log("warning", stdout, "\033[33m" _format "\033[0m", ##__VA_ARGS__)
+        __log("warning", stdout, "\033[33m" _format "\033[0m", ##__VA_ARGS__)
 
 #endif
 
