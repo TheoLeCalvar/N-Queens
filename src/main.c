f64f536a12491cd916062faeb13a082784eca07a
Théo Le Calvar <theo.lecalvar@gmail.com>
2015-03-22 16:49:08 +0100

    New options concerning validation and printing the result

--- src/main.c
+++ src/main.c
@@ -53,9 +53,11 @@ void algo_help(void) {
 void    usage() {
         printf("usage: n-dames [options] size\n"
                "options:\n"
-               " --help\tdisplay this message\n"
-               " --algo=<algo>\tuse specified the algorithm\n"
+               " --help \tdisplay this message\n"
                " --image=path\texport chessboard configuration to bmp image\n"
+               " --noprint\tdo not print the solution, printing solution for big number may be very slow\n"
+               " --check\tcheck if the solution is valid, it is slow for big numbers\n"
+               " --algo=<algo>\tuse specified the algorithm\n"
                " algorithms are:\n");
         algo_help();
 }
@@ -65,18 +67,24 @@ struct option options[] = {
         {"help",        0, NULL, 'h'},
         {"algo",        1, NULL, 'a'},
         {"image",       0, NULL, 'i'},
+        {"check",       0, NULL, 'c'},
+        {"noprint",     0, NULL, 'p'},
         {0,             0, NULL, 0}
 };
 
 int main(int argc, char** argv) {
         int     opt;
         int     algo = 0;
+        char    check = 0;
+        char    print = 0;
         char*   path = NULL;
         size_t  size = 0;
         cb_t    res;
         u32     queens[MAX_QUEENS];
 
-        while ((opt = getopt_long(argc, argv, "a:i:h", options, NULL)) >= 0) {
+        srand(time(NULL));
+
+        while ((opt = getopt_long(argc, argv, "a:i:hcp", options, NULL)) >= 0) {
 
                 switch (opt) {
                         case 'h':
@@ -96,6 +104,14 @@ int main(int argc, char** argv) {
                                 path = optarg;
                                 break;
 
+                        case 'c':
+                                check = 1;
+                                break;
+
+                        case 'p':
+                                print = 1;
+                                break;
+
                         case '?':
                                 usage();
                                 return 1;
@@ -108,24 +124,24 @@ int main(int argc, char** argv) {
         }
 
         if (argc < 2) {
-                printf("wrong number of parameters\n");
+                log_warn("wrong number of parameters\n");
                 usage();
                 return 1;
         }
 
         if (algos[algo].func == NULL) {
-                printf("algorithm not yet implement.\n");
+                log_warn("algorithm not yet implement.\n");
                 return 1;
         }
 
         size = atoi(argv[argc - 1]);
 
         if (size == 1) {
-                printf("just put the queen in the only available slot.\n");
+                log_info("just put the queen in the only available slot.\n");
                 return 1;
         }
         else if (size <= 3) {
-                printf("there is no solutions, size needs to be >= 3.\n");
+                log_info("there is no solutions, size needs to be >= 3.\n");
                 return 1;
         }
 
@@ -148,15 +164,26 @@ int main(int argc, char** argv) {
         }
 
         if (algos[algo].func(&res)) {
-                printf("No solution or an error occured, i'm a sad panda\n");
+                log_err("No solution or an error occured, i'm a sad panda\n");
                 return 1;
         }
 
-        cb_print(&res);
+        if (print) {
+                log_info("Solution not printed");
+        }
+        else {
+                cb_print(&res);
+        }
         // cb_display(&res);
 
-        if (cb_validates_full(&res)) {
-                log_err("The solution isn't valid, this should not happen");
+        if (check) {
+                if (!cb_validates_full(&res))
+                        log_info("The solution is valid.");
+                else
+                        log_err("The solution isn't valid, this should not happen");
+        }
+        else {
+                log_info("No validation performed, solution may not be valid (but that is unlikely).");
         }
 
 
