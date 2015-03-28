ceba9671e346c9facec6f7bb3f987ed3a928da72
alex <alexandrelepettidu49@hotmail.fr>
2015-03-29 00:11:37 +0100

    version final sans bug , amazing

--- src/local_search2.c
+++ src/local_search2.c
@@ -1,130 +1,6 @@
 #include "local_search2.h"
 #include <time.h>
-typedef struct s_List List;
-	struct s_List {
-		List *next;
-		int qconf;
-	};
 
-	//List *list = NULL;
-
-	List * list_create(int q) {
-		List * list= malloc(sizeof(list));
-		if (list)
-		{
-			list->qconf = q;
-			list->next = NULL;
-		}
-		return list;
-	}
-
-	List * list_adhead(List * old,int data) {
-		List *list = list_create(data);
-		if(list)
-			list->next = old;
-		return list;
-	}
-
-	List * list_append(List *list, int data) {
-	    List **plist = &list;
-	    while (*plist)
-	       plist = &(*plist)->next;
-	    *plist = list_create(data);
-	    if (*plist)
-	       return list;
-	    else
-	       return NULL;
-	}
-	List * list_remove_first(List *list) {
-		List *first = list;
-		list = list->next;
-		free(first);
-		return list;
-	}
-	void list_destroy(List *list) {
-		while (list)
-			list = list_remove_first(list);
-	}
-	int list_length(List *list) {
-		int length = 0;
-		while (list) 
-		{
-			length++;
-			list = list->next;
-		}
-		return length;
-	}
-	void list_affiche(List * list) {
-		while (list) {
-			//printf("hello world ");
-			printf("%d ",list->qconf);
-			list = list->next;
-
-		}
-	}
-	int cb_conflict_list(const cb_t * cb,List ** tabli) {
-		if (!cb->size) {
-                log_err("Call cb_conflicts on non valid cb");
-                return -1;
-        }
-        int conflict = 0;
-        for (size_t i = 0; i < cb->size; i++)
-        	if (cb->queens[i] != -1)
-        		for (size_t j = i+1; j < cb->size; ++j)
-        			if (cb->queens[j] != -1 &&
-                    	(cb->queens[i] == cb->queens[j] ||
-                        abs(i - j) == abs(cb->queens[i] - cb->queens[j]))
-                    ) {
-             
-        					tabli[i] = list_adhead(tabli[i],j);
-        					tabli[j] = list_adhead(tabli[j],i);
-        				
-        					++conflict;
-        			}
-
-        return conflict;
-	}
-	int cb_conflicts_point_list(const cb_t * cb, List ** tabli, size_t ligne, size_t col) {
-		int conflict = 0;
-		u32 queen; 
-		for (size_t i = 0; (i < cb->size) && ((queen = cb->queens[i]) != -1); ++i) {
-                if (    i != col &&
-                        abs(i - col) == abs(queen - ligne)
-                   ) {
-                   	tabli[i] = list_adhead(tabli[i],col);
-                   	tabli[col] = list_adhead(tabli[col],i);
-                        ++conflict;
-                   }
-        }
-
-        return conflict;
-	}
-	bool list_isinside(List * list, int x) {
-		while (list) {
-			if (list->qconf == x)
-				return true;
-			else list = list->next;
-		}
-		return false; 
-	}
-	void init_test(cb_t * cb)//debug
-	{
-
-			size_t queens[10]={ 0, 5, 4, 1, 2, 3, 9, 7, 8, 6};
-			for (int i=0; i < 10 ; i++) {
-				cb->queens[i] = queens[i];
-			}
-			
-
-		
-	}
-	/*size_t dame_max_conflit(size_t taille, List **tablist) {
-		size_t max = 0;
-		for (size_t i = 0; i < taille ; i++) {
-			if(list_length(tablist[i]) > max)
-				max = i;
-		}
-	}*/
 	void init_diagonal(const cb_t * cb,size_t * diag_pos,size_t * diag_neg,size_t * conflict_pos, size_t * conflict_neg) {
 		for(size_t i=0; i < cb->size ; i++) {
 			
@@ -257,13 +133,18 @@ int local_search2(cb_t * cb) {
 	printf("l'initialisation est terminé \n");
 	
 	srand(time(NULL));
+
+	size_t * diag_pos = malloc((size * 2) * sizeof(size_t));
+	size_t * diag_neg = malloc((size * 2) * sizeof(size_t));
+	size_t * conflict_pos = malloc((size * 2) * sizeof(size_t));
+	size_t * conflict_neg = malloc((size * 2) * sizeof(size_t));
 	//List * tablist[cb->size];
-	size_t diag_pos[(size * 2)-1];
-	size_t diag_neg[(size * 2)-1];
+	//size_t diag_pos[(size * 2)-1];
+	//size_t diag_neg[(size * 2)-1];
 
 
-	size_t conflict_pos[(size * 2)-1];
-	size_t conflict_neg[(size * 2)-1];
+	//size_t conflict_pos[(size * 2)-1];
+	//size_t conflict_neg[(size * 2)-1];
 
 
 
@@ -282,33 +163,9 @@ int local_search2(cb_t * cb) {
 	}
 
 	init_diagonal(cb, diag_pos, diag_neg, conflict_pos, conflict_neg); 
-	//affiche_diag(size,diag_pos);
-	//for (size_t i = 0; i < cb->size; i++) {
-	//	tablist[i] = NULL;
-	//}
-	//u32 * buf ;
-	//buf = malloc(cb->size *sizeof(u32));
-	//int c = 0;
-	//int c3 = cb_conflicts(cb,buf);
-	//c = cb_conflict_list(cb,tablist);
+
 	size_t c2 = nb_conflict(size*2 -1 ,conflict_neg,conflict_pos);
-	//cb_print(cb);
-	//printf("diagpos \n");
-	//affiche_diag(size*2 -1,diag_pos);
-	//printf("diagneg \n");
-	//affiche_diag(size*2 -1,diag_neg);
-	//printf("confli_pos \n");
-	//affiche_diag(size*2 -1,conflict_pos);
-	//printf("conflig_neg \n");
-	//affiche_diag(size*2-1,conflict_neg);
-	//if (c != c2)
-	//	printf("eu probleme mon capitain ? %d , %d  , %d \n",c,c2,c3);
-	/*for (size_t i = 0; i < cb->size; ++i) {
-		printf("colone numero %d \n",i);
-		list_affiche(tablist[i]);
-	}*/
-	//printf("ok ? \n");
-	//size_t dame_actuel = 0;
+
 	unsigned long int swap =0;
 	while (c2 > 0) {
 			size_t dame_actuel = /*dame_max_conflit(cb->size,tablist)*/rand()%(cb->size);
@@ -353,6 +210,11 @@ int local_search2(cb_t * cb) {
 		printf("il y a eu %lu swap mon captain \n",swap);
 		
 		//int c1 = cb_conflicts(cb,buf);
+		free(diag_pos);
+		free(diag_neg);
+		free(conflict_pos);
+		free(conflict_neg);
+
 
 		
 	
